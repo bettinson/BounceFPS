1b078ecd4b02a62f64d0a772f45aa441a70f1a09
Matt Bettinson <mattbettinson@me.com>
2017-03-24 11:29:51 -0400

    Player now bouncing

---
+++ Assets/BounceOnFloorCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BounceOnFloorCollision : MonoBehaviour {
+	
+	void OnCollisionEnter (Collision col)
+	{
+//		print ("HEY");
+//		Destroy(col.gameObject);
+
+		if(col.gameObject.name == "Capsule")
+		{
+			print ("Found capsule");
+//			print ("HEY");
+//			GameObject player = GameObject.Find (col.gameObject.name);
+//			player.transform.position.Set (0, 0, 10);
+
+		}
+	}
+}
